5e65aa73d7e59a0e8f29e9dc70a04450f8ead010
caiqian <zero530@126.com>
2015-10-16 17:00:19 +0800

    init

---
+++ MVC5/Models/MaintenanceLogDetail.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MMSWebApp.Models
+{
+    public class MaintenanceLogDetail
+    {
+        private string desc;
+
+        public int id { get; set; }
+        public int OriginID { get; set; }
+        public int SMEID { get; set; }
+        [Display(Name = "FA")]
+        public string FunctionArea { get; set; }
+        public string System { get; set; }
+        [Display(Name = "SER/Temp")]
+        public string SERNoTempNo { get; set; }
+        [Display(Name = "Req Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime Requested_Date { get; set; }
+        [Display(Name = "Req By")]
+        public string Requested_By { get; set; }
+
+        [Display(Name = "Desc")]
+        [DataType(DataType.MultilineText)]
+        public string IssueDescription
+        {
+            get
+            {
+                if (desc.Length > 150)
+                    return desc.Substring(0, 150) + "...";
+                else
+                    return desc;
+            }
+
+            set { desc = value; }
+            }
+        public string Priority { get; set; }
+        public string Severity { get; set; }
+        public decimal Estimated { get; set; }
+        [Display(Name = "Category")]
+        public string IssueCategory { get; set; }
+        public string Status { get; set; }
+
+        [Display(Name = "actBy")]
+        public string ActionedBy { get; set; }
+
+        [Display(Name = "BaselineStart Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BaselineStart { get; set; }
+        [Display(Name = "BaselineFinish Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime BaselineFinish { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime RevisedStart { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime RevisedFinish { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ActualStart { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ActualFinish { get; set; }
+        [Display(Name = "actMDs")]
+        public decimal ActualMandays { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime LastUpdatedDate { get; set; }
+        public string LastUpdatedBy { get; set; }
+        public string Remarks { get; set; }
+        public string Type { get; set; }
+        public string ImportDesc { get; set; }
+
+    }
+
+    public class MaintenanceLogDetailDBContext : DbContext
+    {
+        public DbSet<MaintenanceLogDetail> MaintenanceLogDetails { get; set; }
+    }
+}
---
+++ MVC5/Models/SMEMatrix.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MMSWebApp.Models
+{
+    public class SMEMatrix
+    {
+        public int ID { get; set; }
+        public string FunctionArea { get; set; }
+        public string System { get; set; }
+        public string Type { get; set; }
+        public string APJSME1 { get; set; }
+        public string APJSME2 { get; set; }
+        public string APJSME3 { get; set; }
+        public string APJSME4 { get; set; }
+        public string MTRTeam { get; set; }
+        public string MTRSME1 { get; set; }
+        public string MTRSME2 { get; set; }
+        public string MTRSME3 { get; set; }
+        public string MTRSME4 { get; set; }
+        public string Remark { get; set; }
+
+    }
+
+    public class SMEMatrixDBContext : DbContext
+    {
+        public DbSet<SMEMatrix> SMEMatrixs { get; set; }
+    }
+}
